505f32329ebc04b8013b719b2d6f9332ff337b5b
Elliot Tormey <elliot.tormey@gmail.com>
2017-04-27 21:27:42 +0100

    fix indentation

--- notify.py
+++ notify.py
@@ -21,7 +21,7 @@ build_url = os.environ('WERCKER_APPLICATION_URL')
 if result == 'failed':
     color = "#900"
 else:
-color = '#36a64f'
+    color = '#36a64f'
 
 message = {
     "channel": '@elliot',
