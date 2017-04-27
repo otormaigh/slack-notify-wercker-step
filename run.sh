b157f6cdd80141328c18afa6cc3761cde84cf4b5
Elliot Tormey <elliot.tormey@gmail.com>
2017-04-27 17:51:36 +0100

    add pip install

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 
 sudo apt-get update -y
 sudo apt-get install -y python2.7
+sudo apt-get install -y pip
+
 echo "python version $(python --version) running"
+echo "pip version $(pip --version) running"
 
 python notify.py
