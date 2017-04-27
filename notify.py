4bac4b2f1e55bd6fd448f8d3047b80f31df07a8d
Elliot Tormey <elliot.tormey@gmail.com>
2017-04-27 21:19:11 +0100

    comment out WERCKER_RESULT call, its only for after-steps

--- notify.py
+++ notify.py
@@ -17,13 +17,13 @@ webhook_url = "https://hooks.slack.com/services/%s" % (webhook_token)
 
 print("BUILD MESSAGE")
 branch = os.environ['WERCKER_GIT_BRANCH']
-result = os.environ['WERCKER_RESULT']
+#result = os.environ['WERCKER_RESULT']
 url = os.environ('WERCKER_APPLICATION_URL')
 
-if result == 'failed':
-    color = "#900"
-else:
-    color = '#36a64f'
+#if result == 'failed':
+    #color = "#900"
+#else:
+color = '#36a64f'
 
 message = {
     "channel": '@elliot',
