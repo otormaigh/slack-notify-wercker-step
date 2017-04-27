742a359df2e21eb7227a89a265a44f30a202a8fc
Elliot Tormey <elliot.tormey@gmail.com>
2017-04-27 22:13:37 +0100

    add build time to slack message

--- notify.py
+++ notify.py
@@ -2,6 +2,32 @@
 import json
 import os
 import requests
+import calendar
+import datetime
+
+def get_elapsed_time():
+    started = int(os.environ['WERCKER_MAIN_PIPELINE_STARTED'])
+    now = datetime.datetime.utcnow()
+    finished = calendar.timegm(now.utctimetuple())
+    elapsed = finished - started
+    return hours_minutes(elapsed)
+
+def hours_minutes(seconds):
+    m, s = divmod(seconds, 60)
+    # parts = []
+    mins = pluralize(m, 'minute')
+    secs = pluralize(s, 'second')
+    if mins:
+        return mins + ' ' + secs
+    return secs
+
+def pluralize(number, word):
+    if number:
+        phrase = '%s %s' % (number, word)
+        if number != 1:
+            phrase += 's'
+        return phrase
+    return ''
 
 webhook_token = os.environ['WEBHOOK_TOKEN']
 webhook_url = "https://hooks.slack.com/services/%s" % (webhook_token)
@@ -18,7 +44,6 @@ user = "@%s" % (os.environ['WERCKER_SLACK_NOTIFY_USER'])
 print("BUILD MESSAGE")
 branch = os.environ['WERCKER_GIT_BRANCH']
 result = os.environ['WERCKER_RESULT']
-build_url = os.environ['WERCKER_RUN_URL']
 
 if result == 'failed':
     color = "#900"
@@ -28,7 +53,7 @@ else:
 message = {
     "channel": user,
     "author_icon": author_icon,
-    "text": "%s build %s" % (os.environ['WERCKER_GIT_REPOSITORY'], result),
+    "text": "%s build %s in %s" % (os.environ['WERCKER_GIT_REPOSITORY'], result, get_elapsed_time()),
     "username": "Wercker",
     "attachments": [
         {
@@ -43,11 +68,6 @@ message = {
                 {
                     "title": "Started by",
                     "value": os.environ['WERCKER_STARTED_BY']
-                },
-                {
-                    "title": "URL",
-                    "value": build_url,
-                    "short": False
                 }
             ]
         },
