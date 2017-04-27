0bd40f547a6e75f806e01637ec9ecada4795dc07
Elliot Tormey <elliot.tormey@gmail.com>
2017-04-27 17:58:03 +0100

    remove pip, change python 2.7 dependency to python

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 sudo apt-get update -y
-sudo apt-get install -y python2.7
-sudo apt-get install -y pip
+sudo apt-get install -y python
 
 echo "python version $(python --version) running"
-echo "pip version $(pip --version) running"
 
 python notify.py
