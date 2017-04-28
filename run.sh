7af328316d06f4a4d518d1e36b1a09b43224fb9d
Elliot Tormey <elliot.tormey@gmail.com>
2017-04-28 12:11:14 +0100

    remove python dependency

--- run.sh
+++ run.sh
@@ -4,11 +4,7 @@ if [ ! "${WEBHOOK_TOKEN}" ]; then
     fail "No webhook token has been set, skipping this step."
 fi
 
-sudo apt-get update -y
-sudo apt-get install -y python
-sudo apt-get install -y python-pip
-
-# Install required modules.
+# Install required python modules.
 sudo pip install requests
 
 python "$WERCKER_STEP_ROOT/notify.py"
